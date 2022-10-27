b23770b32db6cd9b5c9ec32831b134262c593bd4
cc <caobo5555@gmail.com>
2022-10-27 22:04:07 +0800

    add radix sort

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,15 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+//打印数组
+void printVec(const vector<int> &nums)
+{
+    for (auto i : nums)
+    {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
 namespace InsertSort
 {
     //插入排序，最差复杂度N2,最好复杂度N
@@ -119,7 +127,7 @@ namespace QuickSort
 namespace HeapSort
 {
     // heapInsert过程，将所给的位置的数，如果其比父节点大则往上移动
-    void heapInsert(vector<int> nums, int index)
+    void heapInsert(vector<int> &nums, int index)
     {
         //如果当前数比父亲节点大，则循环，直到比它小或者到了根部，此时index = 0,(index -1)/2 = 0,条件不满足也会结束循环
         while (nums[index] > nums[(index - 1) / 2])
@@ -128,8 +136,8 @@ namespace HeapSort
             index = (index - 1) / 2;
         }
     }
-    //当一个大根堆的某个节点被替换掉时候，将其重新变成大根堆的过程
-    void heapify(vector<int> nums, int index, int heapSize)
+    //当一个大根堆的某个节点被变小的时候，将其重新变成大根堆的过程
+    void heapify(vector<int> &nums, int index, int heapSize)
     {
         int left = index * 2 + 1; //其左孩子的节点
 
@@ -138,31 +146,116 @@ namespace HeapSort
         {
             //先找到左右孩子当中最大的那一个
             int biggest = (nums[left] < nums[left + 1]) && (left + 1 < heapSize) ? (left + 1) : left;
+
+            //如果孩子比节点处值小，直接退出
             if (nums[biggest] <= nums[index])
             {
                 break;
             }
+
+            swap(nums[biggest], nums[index]);
+            index = biggest;
+            left = index * 2 + 1;
+        }
+    }
+
+    void sort(vector<int> &nums)
+    {
+        //将其构造成一个大根堆
+        for (int i = 0; i < nums.size(); i++)
+        {
+            heapInsert(nums, i);
+        }
+        for (int heapSize = nums.size(); heapSize > 0; heapSize--)
+        {
+            //将堆的最后一个位置和根部（最大值）做交换
+            swap(nums[0], nums[heapSize - 1]);
+            heapify(nums, 0, heapSize - 1);
         }
     }
 
 } // HeapSort
-//打印数组
-void printVec(const vector<int> &nums)
+
+//基数排序，属于桶排序，目前只能处理全部都是大于0的整数
+namespace RadixSort
 {
-    for (auto i : nums)
+    int maxBits(const vector<int> &nums, int L, int R)
     {
-        cout << i << " ";
+        int max_num = 0;
+        for (int i = L; i <= R; i++)
+        {
+            max_num = max(nums[i], max_num);
+        }
+        if (max_num == 0)
+            return 1;
+        int res = 0;
+        while (max_num != 0)
+        {
+            max_num /= 10;
+            res++;
+        }
+        return res;
     }
-    cout << "\n";
-}
+    int getDigit(int num, int d)
+    {
+        while (d > 1)
+        {
+            num /= 10;
+            d--;
+        }
+        return num % 10;
+    }
+
+    void sortProcess(vector<int> &nums, int L, int R, int digit)
+    {
+        vector<int> buckets;
+        buckets.resize(R - L + 1);
+
+        for (int d = 1; d <= digit; d++)
+        {
+            vector<int> counts;
+            counts.resize(10);
+
+            int j = 0; //表示第d位是几
+
+            //统计第d位的个数
+            for (int i = L; i <= R; i++)
+            {
+                j = getDigit(nums[i], d);
+                counts[j]++;
+            }
+
+            //生成前缀和的数组
+            for (int i = 1; i < counts.size(); i++)
+            {
+                counts[i] = counts[i - 1] + counts[i];
+            }
+            //将数据出桶
+            for (int i = R; i >= L; i--)
+            {
+                j = getDigit(nums[i], d);
+                buckets[counts[j] - 1] = nums[i];
+                counts[j]--;
+            }
+
+            //将排好序的数组返回到数组中去
+            for (int i = L, b = 0; i <= R; i++, b++)
+            {
+                nums[i] = buckets[b];
+            }
+        }
+    }
+
+    void sort(vector<int> &nums)
+    {
+        sortProcess(nums, 0, nums.size() - 1, maxBits(nums, 0, nums.size() - 1));
+    }
+} // RadixSort
 
 int main()
 {
-    vector<int> nums = {6, 3, 5, 2, 3, 4};
-    for (int i = 0; i < nums.size(); i++)
-    {
-        HeapSort::heapInsert(nums, i);
-    }
+    vector<int> nums = {3, 5, 9, 4, 6, 7, 0};
+    RadixSort::sort(nums);
     printVec(nums);
     return 0;
 }
